01773e7ba3c9e595608daa3b9f75c2b4604e8d28
abarcaarguello <134571936+abarcaarguello@users.noreply.github.com>
2023-07-02 17:21:07 -0600

    TAREA
    TAREA

---
+++ Joseeduardo/Joseeduardo/flogin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Joseeduardo
+{
+    public partial class flogin : Form
+    {
+        public flogin()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtusuario.Clear();
+            txtcontraseña.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtusuario.Text == "Jose" && txtcontraseña.Text == "lopez")
+            {
+                finicio finicio = new finicio();
+                this.Hide();
+                finicio.Show();
+
+
+
+            }
+            else
+            {
+                MessageBox.Show("DATOS INCORRECTOS");
+
+            }
+        }
+    }
+}
